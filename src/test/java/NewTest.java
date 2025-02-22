1d2f82628ddd8787aafa8d0c920f9e20851b6fb5
Александр Бондарев <aleksandr_bond@MacBook-Pro-Aleksandr.local>
2025-02-23 00:51:21 +0300

    from idea;2D;2D

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class NewTest {
+
+    @Test
+    public void testTest(){
+        System.out.println("hello world");
+        Assertions.assertTrue(true);
+    }
+
+}
